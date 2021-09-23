e51442b72e66aa2d0df90888e5af15e6d1b2fcad
James Brownie <jamespbrownie@gmail.com>
2021-09-23 08:45:10 -0500

    finished away team

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -16,16 +16,72 @@ function gameObject() {
                     SlamDunks: 1
                 },
                 "Reggie Evans": {
-                    Number: 0,
-                    Shoe: 16,
-                    Points: 22,
+                    Number: 30,
+                    Shoe: 14,
+                    Points: 12,
                     Rebounds: 12,
                     Assists: 12,
-                    Steals: 3,
-                    Blocks: 1,
-                    SlamDunks: 1
+                    Steals: 12,
+                    Blocks: 12,
+                    SlamDunks: 7
                 }
                     
+            }
+        },
+        away: {
+            teamName: "Charlotte Hornets",
+            colors: ["Turquoise", "Purple"],
+            players: {
+                "Jeff Adrien": {
+                    Number: 4,
+                    Shoe: 18,
+                    Points: 10,
+                    Rebounds: 1,
+                    Assists: 1,
+                    Steals: 2,
+                    Blocks: 7,
+                    SlamDunks: 2
+                },
+                "Bismak Biyombo": {
+                    Number: 0,
+                    Shoe: 16,
+                    Points: 12,
+                    Rebounds: 4,
+                    Assists: 7,
+                    Steals: 7,
+                    Blocks: 15,
+                    SlamDunks: 10
+                }, 
+                "DeSagna Diop": {
+                    Number: 2,
+                    Shoe: 14,
+                    Points: 24,
+                    Rebounds: 12,
+                    Assists: 12,
+                    Steals: 4,
+                    Blocks: 5,
+                    SlamDunks: 5
+                },
+                "Ben Gordon": {
+                    Number: 8,
+                    Shoe: 15,
+                    Points: 33,
+                    Rebounds: 3,
+                    Assists: 2,
+                    Steals: 1,
+                    Blocks: 1,
+                    SlamDunks: 0
+                },
+                "Brendan Haywood": {
+                    Number: 33,
+                    Shoe: 15,
+                    Points: 6,
+                    Rebounds: 12,
+                    Assists: 12,
+                    Steals: 22,
+                    Blocks: 5,
+                    SlamDunks: 12
+                }
             }
         }
     }
